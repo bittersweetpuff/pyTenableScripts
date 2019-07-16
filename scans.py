7e2469071f3fa935b296a4ffb68f72203d89f86b
SKOWRONEK <JAN.SKOWRONEK@ing.com>
2019-07-16 15:45:31 +0200

    More methods done in scans.py module. Almost done there

--- scans.py
+++ scans.py
@@ -14,211 +14,155 @@ from base import TypeCheck
 class Scans:
     """namespace as scans of Nessus"""
 
-
     def _constructor(self, **kw):
-            '''
-            Handles parsing the keywords and returns a scan definition document
-            '''
-            if 'name' in kw:
-                # simply verify that the name attribute is a string.
-                TypeCheck('name', kw['name'], str)
+        """
+            Handles parsing the keywords and returns a scan settings document
+            """
+        if "name" in kw:
+            TypeCheck("name", kw["name"], str)
 
-            if 'type' in kw:
-                # If the scan type is manually specified, then we will want to make
-                # sure that its a valid input.
-                TypeCheck('type', kw['type'], str, choices=['plugin', 'policy'])
+        if "description" in kw:
+            TypeCheck("description", kw["description"], str)
 
-            if 'description' in kw:
-                # The description should always be a string value.
-                TypeCheck('description', kw['description'], str)
+        if "policy_id" in kw:
+            TypeCheck("policy_id", kw["policy_id"], int)
 
-            if 'repo' in kw:
-                # as we accept input as a integer, we need to expand the repository
-                # attribute to be a dictionary item with just the ID (per API docs)
-                kw['repository'] = {'id': TypeCheck(
-                    'repo', kw['repo'], int)}
-                del(kw['repo'])
+        if "folder_id" in kw:
+            TypeCheck("folder_id", kw["folder_id"], int)
 
-            if 'scan_zone' in kw:
-                # similarly to the repository, the API expects the zone to be
-                # defined as a sub-dictionary with just the id field.
-                kw['zone'] = {'id': TypeCheck(
-                    'scan_zone', kw['scan_zone'], int, default=0)}
-                del(kw['scan_zone'])
+        if "scanner_id" in kw:
+            TypeCheck("scanner_id", kw["scanner_id"], int)
 
-            if 'email_complete' in kw:
-                # As emailOnFinish is effectively a string interpretation of a bool
-                # value, if the snake case equivalent is used, we will convert it
-                # into the expected parameter and remove the snake cased version.
-                kw['emailOnFinish'] = str(TypeCheck(
-                    'email_complete', kw['email_complete'], bool, default=False)).lower()
-                del(kw['email_complete'])
+        if "enabled" in kw:
+            TypeCheck("enabled", kw["enabled"], bool)
 
-            if 'email_launch' in kw:
-                # As emailOnLaunch is effectively a string interpretation of a bool
-                # value, if the snake case equivalent is used, we will convert it
-                # into the expected parameter and remove the snake cased version.
-                kw['emailOnLaunch'] = str(TypeCheck(
-                    'email_launch', kw['email_launch'], bool, default=False)).lower()
-                del(kw['email_launch'])
+        if "launch" in kw:
+            TypeCheck("launch", kw["launch"], str)
 
-            if 'timeout' in kw:
-                # timeout is the checked version of timeoutAction.  If timeout is
-                # specified, we will check to make sure that the action is a valid
-                # one, put the result into timeoutAction, and remove timeout.
-                kw['timeoutAction'] = TypeCheck('timeout', kw['timeout'], str,
-                    choices=['discard', 'import', 'rollover'], default='import')
-                del(kw['timeout'])
+        if "starttime" in kw:
+            TypeCheck("starttime", kw["starttime"], str)
 
-            if 'vhosts' in kw:
-                # As scanningVirtualHosts is effectively a string interpretation of
-                # a bool value, if the snake case equivalent is used, we will
-                # convert it into the expected parameter and remove the snake cased
-                # version.
-                kw['scanningVirtualHosts'] = str(TypeCheck(
-                    'vhosts', kw['vhosts'], bool, default=False)).lower()
-                del(kw['vhosts'])
+        if "rrules" in kw:
+            TypeCheck("rrules", kw["rrules"], str)
 
-            if 'rollover' in kw:
-                # The scan rolloverType parameter simply shortened to better conform
-                # to pythonic naming convention.
-                kw['rolloverType'] = TypeCheck('rollover', kw['rollover'], str,
-                    choices=['nextDay', 'template'], default='template')
-                del(kw['rollover'])
+        if "timezone" in kw:
+            TypeCheck("timezone", kw["timezone"], str)
 
-            if 'targets' in kw:
-                # targets is list representation of a comma-separated string of
-                # values for the ipList attribute.  By handling as a list instead of
-                # the raw string variant the API expects, we can ensure that there
-                # isn't any oddities, such as extra spaces, between the commas.
-                kw['ipList'] = ','.join([TypeCheck('target', i.strip(), str)
-                    for i in TypeCheck('targets', kw['targets'], list)])
-                del(kw['targets'])
+        if "text_targets" in kw:
+            TypeCheck("text_targets", kw["text_targets"], str)
 
-            if 'max_time' in kw:
-                # maxScanTime is a integer encased in a string value.  the snake
-                # cased version of that expects an integer and converts it into the
-                # string equivalent.
-                kw['maxScanTime'] = str(TypeCheck('max_time', kw['max_time'], int))
-                del(kw['max_time'])
+        if "agent_group_id" in kw:
+            TypeCheck("agent_group_id", kw["agent_group_id"], list)
 
-            if 'auto_mitigation' in kw:
-                # As classifyMitigatedAge is effectively a string interpretation of
-                # an int value, if the snake case equivalent is used, we will
-                # convert it into the expected parameter and remove the snake cased
-                # version.
-                kw['classifyMitigatedAge'] = str(TypeCheck(
-                    'auto_mitigation', kw['auto_mitigation'], int, default=0)).lower()
-                del(kw['auto_mitigation'])
+        if "file_targets" in kw:
+            TypeCheck("file_targets", kw["file_targets"], str)
 
-            # hand off the building the schedule sub-document to the schedule
-            # document builder.
-            if 'schedule' in kw:
-                kw['schedule'] = self._schedule_constructor(kw['schedule'])
+        if "emails" in kw:
+            TypeCheck("emails", kw["emails"], str)
 
-            if 'reports' in kw:
-                # as the reports list should already be in a format that the API
-                # expects, we will simply verify that everything looks like it should.
-                for item in TypeCheck('reports', kw['reports'], list):
-                    TypeCheck('report:id', item['id'], int),
-                    TypeCheck('reportSource', item['reportSource'], str, choices=[
-                        'cumulative',
-                        'patched',
-                        'individual',
-                        'lce',
-                        'archive',
-                        'mobile'
-                    ])
+        if "acls" in kw:
+            TypeCheck("acls", kw["acls"], list)
 
-            if 'asset_lists' in kw:
-                # asset_lists is the collapsed list of id documents that the API
-                # expects to see.  We will check each item in the list to make sure
-                # its in the right type and then expand it into a sub-document.
-                kw['assets'] = [{'id': TypeCheck('asset_list:id', i, int)}
-                    for i in TypeCheck('assets_lists', kw['asset_lists'], list)]
-                del(kw['asset_lists'])
-
-            if 'creds' in kw:
-                # creds is the collapsed list of id documents that the API expects
-                # to see.  We will check each item in the list to make sure its in
-                # the right type and then expand it into a sub-document.
-                kw['credentials'] = [{'id': TypeCheck('cred:id', i, int)}
-                    for i in TypeCheck('creds', kw['creds'], list)]
-                del(kw['creds'])
-
-            # Lastly, we need to handle the scan types automatically...
-            if 'plugin_id' in kw and 'policy_id' in kw:
-                # if both are specified, something is wrong here and we should throw
-                # an exception.
-                raise UnexpectedValueError(
-                    'specify either a plugin_id or a policy_id for a scan, not both.')
-
-            elif 'plugin_id' in kw:
-                # If just the plugin_id is specified, then we are safe to assume
-                # that this is a plugin-based scan.  set the pluginID attribute as
-                # the API would expect and remove the snake cased variant that was
-                # inputted.
-                kw['type'] = 'plugin'
-                kw['pluginID'] = TypeCheck('plugin_id', kw['plugin_id'], int)
-                del(kw['plugin_id'])
-
-            elif 'policy_id' in kw:
-                # If just the policy_id is specified, then we are safe to assume
-                # that this is a policy-based scan.  set the policy id attribute
-                # within the policy document as the API would expect and remove the
-                # snake cased variant that was inputted.
-                kw['type'] = 'policy'
-                kw['policy'] = {'id': TypeCheck('policy_id', kw['policy_id'], int)}
-                del(kw['policy_id'])
-
-            return kw
-
+        return kw
 
     def __init__(self, nessus_connection):
         self.nessus = nessus_connection
-
 
     def execute(self, *args, **kwargs):
         """proxy method"""
         return self.nessus.execute(*args, **kwargs)
 
-
     def create(self, name, template_name, targets, **kw):
-        '''
+        """
         Creates a scan definition.
-        '''
-        kw['name'] = name
-        uuid = self.nessus.editor.get_uuid_by_name('scan', template_name)
-        kw['text_targets'] = targets
-        kw['enabled'] = True
+
+        Args:
+            uuid (str):
+                The uuid for the editor template to use.
+            name (str):
+                The name of the scan.
+            description (str, optional):
+                The description of the scan.
+            policy_id (int, optional):
+                The unique id of the policy to use.
+            folder_id (int, optional):
+                The unique id of the destination folder for the scan.
+            scanner_id (int, optional):
+                The unique id of the scanner to use.
+            enabled (bool):
+                If true, the schedule for the scan is enabled.
+            launch (str, optional):
+                When to launch the scan. (i.e. ON_DEMAND, DAILY, WEEKLY, MONTHLY, YEARLY)
+            starttime (str, optional):
+                The starting time and date for the scan (i.e. YYYYMMDDTHHMMSS).
+            rrules (str, optional):
+                Expects a semi-colon delimited string comprised of three values.
+                The frequency (FREQ=ONETIME or DAILY or WEEKLY or MONTHLY or YEARLY),
+                the interval (INTERVAL=1 or 2 or 3 ... x), and the days of the
+                week (BYDAY=SU,MO,TU,WE,TH,FR,SA). To create a scan that runs
+                every three weeks on Monday Wednesday and Friday the string
+                would be 'FREQ=WEEKLY;INTERVAL=3;BYDAY=MO,WE,FR'
+            timezone (str, optional):
+                The timezone for the scan schedule.
+            text_targets (str):
+                The list of targets to scan.
+            agent_group_id (list, optional):
+                The list of agent group IDs to scan; only valid for Agent scans.
+            file_targets (str, optional):
+                The name of a file containing the list of targets to scan.
+            emails (str, optional):
+                A comma separated list of accounts who will recieve the email summary report.
+            acls (list, optional):
+                An array containing permissions to apply to the scan.
+
+        Returns:
+            :obj:`dict`:
+                The scan resource for the created scan.
+
+        Examples:
+            Creating a scan for a single host:
+
+            >>> sc.scans.create('Example scan', 1, policy_id=1001,
+            ...     targets=['127.0.0.1'])
+        """
+        kw["name"] = name
+        uuid = self.nessus.editor.get_uuid_by_name("scan", template_name)
+        kw["text_targets"] = targets
+        kw["enabled"] = True
 
         payload = dict()
-        payload['uuid'] = uuid
-        payload['settings'] = kw
+        payload["uuid"] = uuid
+        payload["settings"] = self._constructor(kw)
 
-
-
-        return self.execute("POST", "/scans", data=payload )
-
-
+        return self.execute("POST", "/scans", data=payload)
 
     def copy(self, scan_id, name=None):
         """
         Clones the scan instance.
         """
-        payload = {
-            'name': name
-        }
+        payload = {"name": name}
 
         cpy = self.execute("POST", f"/scans/{scan_id}/copy", data=payload)
         return cpy
 
-
     def details(self, scan_id, fields=None):
         """
         Returns the details for a specific scan.
-        """
 
+        Args:
+            scan_id (int):
+                The identifier for the scan.
+            fields (list, optional):
+                A list of attributes to return.
+
+        Returns:
+            :obj:`dict`:
+                The alert resource record.
+
+        Examples:
+            >>> details = nessus.scans.detail(10001)
+            >>> pprint(details)
+        """
         scan = self.execute("GET", f"/scans/{scan_id}")
         det = dict()
         if fields:
@@ -229,25 +173,61 @@ class Scans:
         else:
             return scan
 
+    def launch(self, scan_id, alt_targets=None):
+        """
+        Launches a scan definition.
 
+        Args:
+            id (int):
+                The id of the scan to launch.
+            alt_targets (list, optional):
+                If specified, these targets will be scanned instead of the
+                default. Value can be an list where each index is a target, or
+                an list with a single index of comma separated targets.
 
-    def launch(self, scan_id, diagnostic_target=None, diagnostic_password=None):
+        Returns:
+            :obj:`dict`:
+                A scan result resource for the newly launched scan.
 
+        Examples:
+            >>> sc.scans.launch(100001)
+        """
         payload = dict()
-        if diagnostic_target and diagnostic_passwsord:
-            payload['diagnosticTarget'] = TypeCheck(
-                'diagnostic_target', diagnostic_target, str)
-            payload['diagnosticPassword'] = TypeCheck(
-                'diagnostic_password', diagnostic_password, str)
+        if alt_targets:
+            payload["alt_targets"] = alt_targets
 
         return self.execute("POST", f"/scans/{scan_id}/launch", data=payload)
 
-
     def delete(self, scan_id):
+        """
+        Removes the specified scan from Nessus.
 
+        Args:
+            scan_id (int): The identifier for the scan to delete.
+
+        Returns:
+            :obj:`list`:
+                The list of scan id removed.
+
+        Examples:
+            >>> nessus.scans.delete(10001)
+        """
         return self.execute("DELETE", f"/scans/{scan_id}")
 
-
     def list(self, fields=None):
+        """
+        Retrieves the list of scan definitions.
 
+        Args:
+            fields (list, optional):
+                A list of attributes to return for each scan.
+
+        Returns:
+            :obj:`list`:
+                A list of scan resources.
+
+        Examples:
+            >>> for scan in nessus.scans.list():
+            ...     pprint(scan)
+        """
         return self.execute("GET", "/scans")
